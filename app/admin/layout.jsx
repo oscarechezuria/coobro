c7dd6bcfe94d321674be320696d832690165e6e9
oscar <echezuoscar@gmail.com>
2023-12-29 19:36:15 -0400

    Se trabajo en la organización del layout en el admin

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,18 +1,29 @@
-"use client"
 /* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useAuthContext } from '../context/AuthProvider'
-import { getSession } from '../lib/actions'
+import { getSession, signOut } from '../lib/actions'
+import { FaUsers } from "react-icons/fa";
+import { IoLogoUsd } from "react-icons/io";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaUserGear, FaAlignCenter } from "react-icons/fa6";
 import Link from 'next/link'
 
 
 
 export default function layout({children}) {
 
-    const {setSession, session, state, setState} = useAuthContext()
+    const {setSession, state, setState} = useAuthContext()
     const [toggle, setToggle] = useState(true)
+    const [currentPath, setCurrentPath] = useState("")
     const router = useRouter()
+    const getCurrentPath = usePathname()
+
+    useEffect(() => {
+        setCurrentPath(getCurrentPath)
+    },[currentPath, getCurrentPath])
 
 
     useEffect(() =>{
@@ -32,6 +43,32 @@ export default function layout({children}) {
     },[router, setSession, state])
 
 
+    const handleSignOut = () => {
+        signOut()
+        setState(null)
+        setToggle(!toggle)
+    }
+
+
+    const renderCurrentPath = () => {
+        switch (currentPath) {
+            case "/admin":
+                    return "Tus Pagos"
+                break;
+            case "/admin/customers":
+                    return "Tus Clientes"
+                break;
+            case "/admin/bio":
+                    return "Tu Información"
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
 
 
     if (state === 0) {
@@ -47,16 +84,43 @@ export default function layout({children}) {
     if (state === 1) {
         return (
             <main className={`flex flex-col w-full md:flex-row`}>
-                <section className={`${toggle ? "absolute" : "hidden"} w-full bg-white h-screen p-2 md:static md:w-1/6 md:flex`}>
-                    <div className='flex flex-col justify-center items-center h-full w-full'>
-                        <Link href={"/admin/payments"} onClick={() => setToggle(!toggle)}>Cobros</Link>
-                        <Link href={"/admin/customers"} onClick={() => setToggle(!toggle)}>Clientes</Link>
-                        <Link href={"/admin/bio"} onClick={() => setToggle(!toggle)}>Bio</Link>
+                <section className={`${toggle ? "absolute" : "hidden"} w-full bg-white h-screen p-2 border-r-2 border-r-three-500 md:static md:w-1/6 md:flex`}>
+                    <div className='flex flex-col items-center gap-14 pt-20 h-full w-full'>
+                        <h2 className='text-one-500 font-extrabold text-2xl underline underline-offset-8'>COOBRO</h2>
+                        <ul className='flex flex-col gap-4 w-full'>
+                            <Link href={"/admin"} onClick={() => setToggle(!toggle)} className='flex justify-center items-center gap-1 w-full py-2 text-xl rounded-xl hover:bg-slate-100 '>
+                                <div className='flex justify-end w-1/3 mr-2'>
+                                    <IoLogoUsd className='text-two-500 font-bold'/>
+                                </div>
+                                <div className='w-2/3 font-normal'>Cobros</div>
+                            </Link>
+                            <Link href={"/admin/customers"} onClick={() => setToggle(!toggle)} className='flex justify-center items-center gap-1 w-full py-2 text-xl rounded-xl hover:bg-slate-100 '>
+                                <div className='flex justify-end w-1/3 mr-2'>
+                                    <FaUsers className='text-two-500 font-bold'/>
+                                </div>
+                                <div className='w-2/3 font-normal'>Clientes</div>
+                            </Link>
+                            <Link href={"/admin/bio"} onClick={() => setToggle(!toggle)} className='flex justify-center items-center gap-1 w-full py-2 text-xl rounded-xl hover:bg-slate-100 '>
+                                <div className='flex justify-end w-1/3 mr-2'>
+                                    <FaUserGear className='text-two-500 font-bold'/>
+                                </div>
+                                <div className='w-2/3 font-normal' >Bio</div>
+                            </Link>
+                            <Link href={"/"} onClick={handleSignOut} className='flex justify-center items-center gap-1 w-full py-2 text-xl rounded-xl hover:bg-slate-100 '>
+                                <div className='flex justify-end w-1/3 mr-2'>
+                                    <FaArrowCircleLeft className='text-two-500 font-bold'/>
+                                </div>
+                                <div className='w-2/3 font-normal'>Cerrar Sesión</div>
+                            </Link>
+                        </ul>
                     </div>
                 </section>
-                <section className='flex flex-col h-screen bg-slate-500 p-4 md:w-5/6'>
+                <section className='flex flex-col h-screen p-4 md:w-5/6'>
+                            <div className="flex justify-between p-4 items-center h-20 w-full border-b-4">
+                                <h2 className="flex text-xl font-extrabold text-two-500 md:text-3xl">{renderCurrentPath()}</h2>
+                                <FaAlignCenter className='block text-xl md:3xl md:hidden hover:cursor-pointer' onClick={() => setToggle(!toggle)}/>
+                            </div>
                             {children}
-                            <button onClick={() => setToggle(!toggle)}>Changed</button>
                 </section>
             </main>
         )
--- app/page.jsx
+++ app/page.jsx
@@ -9,18 +9,17 @@ export default function Home() {
 
   const {setUser, state, setState, session } = useAuthContext()
   const [open, setOpen] = useState(false)
-  console.log(open)
 
   return (
     <main>
           <header className="border-three-500 border-b-4">
-            <nav className={`${open ? "flex flex-col" : "flex flex-row"} justify-between py-4 px-8 text-2xl md:py-4 md:px-14`}>
+            <nav className={`${open ? "flex flex-col" : "flex flex-row"} justify-between py-4 px-8 text-xl md:py-2 md:px-14`}>
               <div className="flex justify-between w-full items-center">
-                <h2 className="text-three-500 font-bold text-3xl">Coobro</h2>
-                <FaAlignCenter className="block text-3xl md:hidden" onClick={() => setOpen(!open)}/>
+                <h2 className="text-three-500 font-bold text-2xl">Coobro</h2>
+                <FaAlignCenter className="block text-2xl md:hidden" onClick={() => setOpen(!open)}/>
               </div>
               <ul className={`${open ? "block" : "hidden"} font-semibold md:flex md:justify-center md:items-center`}>
-                <div className="flex flex-col justify-between items-center gap-4 mt-8 md:flex md:flex-row md:mt-0">
+                <div className="flex flex-col justify-between items-center gap-4 mt-8 text-base md:flex md:flex-row md:mt-0 md:text-xl">
                   <Link href={"/login"} className={`text-black whitespace-nowrap`}>Iniciar Sesión</Link>
                   <Link href={"/signup"} className={`bg-one-500 rounded-lg p-3 text-white hover:bg-opacity-80`}>Registrarse</Link>
                 </div>
